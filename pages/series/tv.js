f5f81eb01575f456f5970a749ccb675a79c82526
NixonXC <invasionyt19@gmail.com>
2022-09-17 13:40:23 +0530

    Add files via upload

--- pages/series/tv.js
+++ pages/series/tv.js
@@ -93,13 +93,20 @@ function myFunction() {
         document.getElementById("home").style.color = "white"
         document.getElementById("mov2").style.color = "white"
         document.getElementById("se2").style.color = "white"
-        document.getElementById("name").style.backgroundColor = "#1a161a"
-        document.getElementById("name").style.border = "1px solid transparent;"
-        document.getElementById("sea").style.backgroundColor = "#1a161a"
-        document.getElementById("sea").style.border = "1px solid transparent;"
-        document.getElementById("epi").style.backgroundColor = "#1a161a"
-        document.getElementById("epi").style.border = "1px solid transparent;"
-        document.getElementById("menu").style.filter = "invert(100%) sepia(0%) saturate(0%) hue-rotate(13deg) brightness(102%) contrast(102%)"
+        let name = document.getElementById("name")
+        name.style.backgroundColor = "#1a161a"
+        name.style.border = "1px solid transparent;"
+        name.style.color = "#ffff"
+        let sea = document.getElementById("sea")
+        sea.style.backgroundColor = "#1a161a"
+        sea.style.border = "1px solid transparent;"
+        sea.style.color = "#ffff"
+        let epi = document.getElementById("epi")
+        epi.style.backgroundColor = "#1a161a"
+        epi.style.border = "1px solid transparent;"
+        epi.style.color = "#ffff"
+        let menu = document.getElementById("menu")
+        menu.style.filter = "invert(100%) sepia(0%) saturate(0%) hue-rotate(13deg) brightness(102%) contrast(102%)"
     } else {
         document.body.style.backgroundImage = "url(assets/bg.png)"
         document.body.style.color = "black"
