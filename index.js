14f876aa3b8f9078da20bbe35138a6355d897da5
NixonXC <invasionyt19@gmail.com>
2022-09-18 12:47:29 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -14,3 +14,45 @@ document.getElementById("mov").onclick = function() {
 document.getElementById("gt").onclick = function() {
     window.location.href = "pages/series/tv.html"
 }
+document.getElementById("darkmode-toggle").onclick = function() {
+    var checkbox = document.getElementById("darkmode-toggle");
+    localStorage.setItem("val", checkbox.checked);
+    var checked = JSON.parse(localStorage.getItem("val"));
+    if (checked == true){
+        document.body.style.backgroundColor = "#141114"
+        document.body.style.backgroundImage = "none"
+        document.body.style.color = "white"
+        document.getElementById("home").style.color = "white"
+        document.getElementById("mov2").style.color = "white"
+        document.getElementById("se2").style.color = "white"
+    } 
+    else {
+        document.body.style.backgroundImage = "url(assets/bg.png)"
+        document.body.style.color = "black"
+        document.body.style.backgroundColor = "none"
+        document.getElementById("home").style.color = "black"
+        document.getElementById("mov2").style.color = "black"
+        document.getElementById("se2").style.color = "black"
+    }
+}
+var checkbox = document.getElementById("darkmode-toggle");
+checked = JSON.parse(localStorage.getItem("val"));
+console.log(checked)
+checkbox.checked = checked
+if (checked == true) {
+    document.body.style.backgroundColor = "#141114"
+    document.body.style.backgroundImage = "none"
+    document.body.style.color = "white"
+    document.getElementById("home").style.color = "white"
+    document.getElementById("mov2").style.color = "white"
+    document.getElementById("se2").style.color = "white"
+    menu.style.filter = "invert(100%) sepia(0%) saturate(0%) hue-rotate(13deg) brightness(102%) contrast(102%)"
+}
+else {
+    document.body.style.backgroundImage = "url(assets/bg.png)"
+    document.body.style.color = "black"
+    document.body.style.backgroundColor = "none"
+    document.getElementById("home").style.color = "black"
+    document.getElementById("mov2").style.color = "black"
+    document.getElementById("se2").style.color = "black"
+}
--- pages/series/tv.js
+++ pages/series/tv.js
@@ -84,9 +84,11 @@ var menuList = document.getElementById("navMenu");
             menuList.style.maxHeight = "0px"
     }
 }
-function myFunction() {
-    var checkBox = document.getElementById("darkmode-toggle");
-    if (checkBox.checked == true){
+document.getElementById("darkmode-toggle").onclick = function() {
+    var checkbox = document.getElementById("darkmode-toggle");
+    localStorage.setItem("val", checkbox.checked);
+    var checked = JSON.parse(localStorage.getItem("val"));
+    if (checked == true){
         document.body.style.backgroundColor = "#141114"
         document.body.style.backgroundImage = "none"
         document.body.style.color = "white"
@@ -107,7 +109,8 @@ function myFunction() {
         epi.style.color = "#ffff"
         let menu = document.getElementById("menu")
         menu.style.filter = "invert(100%) sepia(0%) saturate(0%) hue-rotate(13deg) brightness(102%) contrast(102%)"
-    } else {
+    } 
+    else {
         document.body.style.backgroundImage = "url(assets/bg.png)"
         document.body.style.color = "black"
         document.body.style.backgroundColor = "none"
@@ -119,4 +122,42 @@ function myFunction() {
         document.getElementById("sea").style.backgroundColor = "#ffff"
         document.getElementById("epi").style.backgroundColor = "#ffff"
     }
-  }
+}
+var checkbox = document.getElementById("darkmode-toggle");
+checked = JSON.parse(localStorage.getItem("val"));
+console.log(checked)
+checkbox.checked = checked
+if (checked == true) {
+    document.body.style.backgroundColor = "#141114"
+    document.body.style.backgroundImage = "none"
+    document.body.style.color = "white"
+    document.getElementById("home").style.color = "white"
+    document.getElementById("mov2").style.color = "white"
+    document.getElementById("se2").style.color = "white"
+    let name = document.getElementById("name")
+    name.style.backgroundColor = "#1a161a"
+    name.style.border = "1px solid transparent;"
+    name.style.color = "#ffff"
+    let sea = document.getElementById("sea")
+    sea.style.backgroundColor = "#1a161a"
+    sea.style.border = "1px solid transparent;"
+    sea.style.color = "#ffff"
+    let epi = document.getElementById("epi")
+    epi.style.backgroundColor = "#1a161a"
+    epi.style.border = "1px solid transparent;"
+    epi.style.color = "#ffff"
+    let menu = document.getElementById("menu")
+    menu.style.filter = "invert(100%) sepia(0%) saturate(0%) hue-rotate(13deg) brightness(102%) contrast(102%)"
+}
+else {
+    document.body.style.backgroundImage = "url(assets/bg.png)"
+    document.body.style.color = "black"
+    document.body.style.backgroundColor = "none"
+    document.getElementById("home").style.color = "black"
+    document.getElementById("mov2").style.color = "black"
+    document.getElementById("se2").style.color = "black"
+    document.getElementById("menu").style.filter = ""
+    document.getElementById("name").style.backgroundColor = "#ffff"
+    document.getElementById("sea").style.backgroundColor = "#ffff"
+    document.getElementById("epi").style.backgroundColor = "#ffff"
+}
